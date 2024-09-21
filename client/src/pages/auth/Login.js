2c621bdc9d130afb8bb4eed091b57e9f7c222537
ali-0507 <araza18183@gmail.com>
2024-09-21 12:56:30 +0530

    Update Login.js

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -12,10 +12,10 @@ const Login = () => {
         <Spinner />
       ) : (
         <div className="row g-0">
-          <div className="col-md-8 form-banner">
+          <div className="col-md-7 form-banner">
             <img src="./assets/images/banner1.jpg" alt="loginImage" />
           </div>
-          <div className="col-md-5 form-container">
+          <div className="col-md-4 form-container">
             <Form
               formTitle={"Login Page"}
               submitBtn={"Login"}
